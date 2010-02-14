990e9cd7007a6844c68b00270bd7930e35cbbd2e
Pavel Repin <prepin@gmail.com>
2010-02-14 03:04:37 -0800

    Minimal pre-forked WSGI server. Doesn't even parse any request headers, it's that minimal.

---
+++ odnorog/experiments/rfc2616.py
@@ -0,0 +1,104 @@
+import collections
+import re
+from urlparse import urlsplit
+
+__all__ = ['RequestLine', 'tokenize_request_line', 'parse_request_line']
+
+RequestLine = collections.namedtuple(
+    'RequestLine',
+    'method uri version parsed_uri')
+
+class ParsingError(ValueError):
+    _sec = re.compile('^sec(\d+).*$')
+    def __init__(self, error, rfc_section):
+        page = self._sec.search(rfc_section).group(1)
+        url = 'http://www.w3.org/Protocols/rfc2616/rfc2616-sec{0}.html#{1}'.format(
+            page,
+            rfc_section)
+        super(ParsingError, self).__init__(error, rfc_section, url)
+
+_request_line = re.compile(
+    r'^(?P<method>[^ ]+) '
+    r'(?P<uri>[^ ]+) '
+    r'(?P<version>HTTP/\d+\.\d+)\r\n$')
+_methods = frozenset(('OPTIONS', 'GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'TRACE', 'CONNECT'))
+
+def tokenize_request_line(stdin):
+    match = _request_line.search(stdin.readline())
+    if match:
+        return match.groupdict()
+    else:
+        raise ParsingError("Request-Line is not: Method SP Request-URI SP HTTP-Version CRLF",
+            'sec5.1')
+
+def parse_request_line(stdin):
+    line = tokenize_request_line(stdin)
+    method = line['method']
+    uri = line['uri']
+    if method not in _methods:
+        raise ParsingError("Method is invalid", 'sec5.1.1')
+    if uri == '*' or method == 'CONNECT':
+        # Don't parse Request-URI: it's either '*' or
+        # 'authority' used by CONNECT method.
+        # See: http://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html#sec5.1.2
+        parsed_uri = None
+    else:
+        parsed_uri = urlsplit(uri, allow_fragments=False)
+        scheme = parsed_uri.scheme
+        path = parsed_uri.path
+        if scheme and scheme not in ('http', 'https'):
+            raise ParsingError("Scheme is neither 'http' nor 'https'", 'sec5.1')
+        if not path or not path.startswith('/'):
+            # TODO: perhaps too draconian? Look for 'abs_path' in following sections.
+            # See: http://www.w3.org/Protocols/rfc2616/rfc2616-sec3.html#sec3.2.1
+            # See: http://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html#sec5.1.2
+            raise ParsingError("Non-absolute URI must be an absulte path", 'sec5.1.2')
+    return RequestLine._make((method, uri, line['version'], parsed_uri))
+
+if __name__ == '__main__':
+    import unittest
+    import cStringIO as sio
+
+    class RequestParsingTests(unittest.TestCase):
+        def test_request_line_tokens(self):
+            stdin = sio.StringIO('qaz wsx HTTP/4.233\r\n')
+            line = RequestLine(parsed_uri=None, **tokenize_request_line(stdin))
+            self.assertEquals(('qaz', 'wsx', 'HTTP/4.233', None), line)
+
+        def test_request_line_tokens_fails(self):
+            for bad in ('PUT /wsx SFTP/25.1\r\n', 'qaz wsx edc azxc\r\n'):
+                stdin = sio.StringIO(bad)
+                self.assertRaises(ParsingError, tokenize_request_line, stdin)
+
+        def test_valid_methods(self):
+            for method in _methods:
+                stdin = sio.StringIO('{0} /wsx HTTP/1.1\r\n'.format(method))
+                if method != 'CONNECT':
+                    self.assertEquals((method, '/wsx', 'HTTP/1.1', urlsplit('/wsx')),
+                                      parse_request_line(stdin))
+                else:
+                    self.assertEquals((method, '/wsx', 'HTTP/1.1', None),
+                                      parse_request_line(stdin))
+
+        def test_invalid_method(self):
+            stdin = sio.StringIO('ZOMG /wsx HTTP/1.1\r\n')
+            self.assertRaises(ParsingError, parse_request_line, stdin)
+
+        def test_wrong_scheme(self):
+            stdin = sio.StringIO('PUT ftp://abc/ HTTP/1.1\r\n')
+            self.assertRaises(ParsingError, parse_request_line, stdin)
+
+        def test_right_scheme(self):
+            for s in ('hTTp', 'HttPS'):
+                stdin = sio.StringIO('PUT {0}://abc/ HTTP/1.1\r\n'.format(s))
+                parse_request_line(stdin)
+
+        def test_no_empty_path(self):
+            stdin = sio.StringIO('GET ?lang=en HTTP/1.1\r\n')
+            self.assertRaises(ParsingError, parse_request_line, stdin)
+
+        def test_no_abs_path(self):
+            stdin = sio.StringIO('GET woopie/boo/baz HTTP/1.1\r\n')
+            self.assertRaises(ParsingError, parse_request_line, stdin)
+
+    unittest.main()
---
+++ odnorog/experiments/wsgi_server.py
@@ -0,0 +1,100 @@
+# Insipred by: http://tomayko.com/writings/unicorn-is-unix
+
+import atexit
+from contextlib import closing
+import os
+import signal
+import socket
+import sys
+import wsgiref.handlers
+from urllib import unquote
+
+from process import fork, trap, wait_all
+from rfc2616 import parse_request_line
+
+class WsgiHandler(wsgiref.handlers.BaseHandler):
+    os_environ = {}
+
+    def __init__(self, stdin, stdout, stderr,
+                 environ, multithread=False, multiprocess=True):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.base_env = environ
+        self.wsgi_multithread = multithread
+        self.wsgi_multiprocess = multiprocess
+
+    def get_stdin(self):
+        return self.stdin
+
+    def get_stderr(self):
+        return self.stderr
+
+    def add_cgi_vars(self):
+        self.environ.update(self.base_env)
+
+    def _write(self,data):
+        self.stdout.write(data)
+        self._write = self.stdout.write
+
+    def _flush(self):
+        self.stdout.flush()
+        self._flush = self.stdout.flush
+
+acceptor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+acceptor.bind(('0.0.0.0', 8888))
+acceptor.listen(1024)
+name = "Parent"
+
+host, server_port = acceptor.getsockname()[:2]
+server_name = socket.getfqdn(host)
+
+@atexit.register
+def on_exit():
+    acceptor.close()
+    print "{0} {1} has left the building.".format(name, os.getpid())
+
+import webob.dec
+from wsgiref.validate import validator
+@webob.dec.wsgify
+def app(req):
+    global name
+    print "{2}: Proudly served by {0} {1}".format(name, os.getpid(), req.path)
+    return "Hello!"
+
+for i in range(3):
+    @fork
+    def child():
+        global name
+        name = "Child"
+
+        @trap(signal.SIGINT)
+        def exit_when_interrupted():
+            sys.exit()
+
+        while True:
+            sock, addr = acceptor.accept()
+            with closing(sock):
+                with closing(sock.makefile('wb', 16384)) as sock_output:
+                    with closing(sock.makefile('rb', 16384)) as sock_input:
+                        request_line = parse_request_line(sock_input)
+                        environ = {
+                            'REQUEST_METHOD': request_line.method,
+                            'PATH_INFO': unquote(request_line.parsed_uri.path),
+                            'QUERY_STRING': request_line.parsed_uri.query,
+                            'SERVER_PROTOCOL': request_line.version,
+                            'SERVER_NAME': server_name,
+                            'SERVER_PORT': str(server_port),
+                            'SCRIPT_NAME': '',
+                        }
+                        handler = WsgiHandler(sock_input, sock_output, sys.stderr, environ)
+                        handler.run(validator(app))
+
+    print "Forked child: {0}".format(child.pid)
+
+@trap(signal.SIGINT)
+def exit_parent():
+    print "\nbailing"
+    sys.exit()
+
+wait_all()
