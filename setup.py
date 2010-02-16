384af23acdf905dbcba07d892e3078009dcc8ab8
Pavel Repin <prepin@gmail.com>
2010-02-15 21:12:08 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Odnorog',
+    version='0.1',
+    author='Pavel Repin',
+    packages=['odnorog', 'odnorog.experiments'],
+)
