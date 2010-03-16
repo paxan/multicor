49c3620f1931dc7751a346b2e58aa93c48b931f8
Pavel Repin <prepin@gmail.com>
2010-03-16 09:25:22 -0700

    Reuse standard HTTP parsing code instead of hacking things by hand.

--- odnorog/experiments/wsgi_server.py
+++ odnorog/experiments/wsgi_server.py
@@ -6,16 +6,34 @@ import os
 import signal
 import socket
 import sys
-import wsgiref.handlers
 import errno
-from urllib import unquote
+import urllib
 from select import select
 
-from process import fork, trap, wait_all
-from rfc2616 import parse_request_line, parse_headers
+from wsgiref.handlers import BaseHandler
+import BaseHTTPServer
 
-class WsgiHandler(wsgiref.handlers.BaseHandler):
-    os_environ = {}
+from process import fork, trap, wait_all
+
+__version__ = '0.1'
+
+_server_version = "odnorog/" + __version__
+_sys_version = "Python/" + sys.version.split()[0]
+_software_version = _server_version + ' ' + _sys_version
+
+def get_server_address(sock):
+    host, server_port = sock.getsockname()[:2]
+    server_name = socket.getfqdn(host)
+    return server_name, server_port
+
+class ServerHandler(BaseHandler):
+    """
+    Lifted from wsgiref.simple_server.ServerHandler and
+    wsgiref.handlers.SimpleHandler.
+    """
+    server_software = _software_version
+
+    os_environ = {} # We're not CGI, don't include system environment vars!
 
     def __init__(self, stdin, stdout, stderr, environ):
         self.stdin = stdin
@@ -42,32 +60,90 @@ class WsgiHandler(wsgiref.handlers.BaseHandler):
         self.stdout.flush()
         self._flush = self.stdout.flush
 
-    @classmethod
-    def parse_request(cls, stdin, stdout, stderr, server_name, server_port):
-        request_line = parse_request_line(stdin)
-        environ = {
-            'REQUEST_METHOD': request_line.method,
-            'PATH_INFO': unquote(request_line.parsed_uri.path),
-            'QUERY_STRING': request_line.parsed_uri.query,
-            'SERVER_PROTOCOL': request_line.version,
-            'SERVER_NAME': server_name,
-            'SERVER_PORT': str(server_port),
-            'SCRIPT_NAME': '',
-        }
-        # Add headers:
-        for name, value in parse_headers(stdin):
-            name = 'HTTP_' + name.replace('-', '_').upper()
-            # TODO: HTTP_CONTENT_TYPE and HTTP_CONTENT_LENGTH are special
-            if name in environ:
-                environ[name] += ',' + value
-            else:
-                environ[name] = value
-        return cls(stdin, stdout, stderr, environ)
+    def close(self):
+        try:
+            self.request_handler.log_request(
+                self.status.split(' ',1)[0], self.bytes_sent
+            )
+        finally:
+            BaseHandler.close(self)
 
-def get_server_address(sock):
-    host, server_port = sock.getsockname()[:2]
-    server_name = socket.getfqdn(host)
-    return server_name, server_port
+
+class WSGIRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    """
+    Copied wsgiref.simple_server.WSGIRequestHandler since
+    it is not very customizable, the way its written,
+    without resorting to monkey-patching.
+    """
+
+    server_version = _server_version
+
+    def get_environ(self):
+        env = self.server.base_environ.copy()
+        env['SERVER_PROTOCOL'] = self.request_version
+        env['REQUEST_METHOD'] = self.command
+        if '?' in self.path:
+            path,query = self.path.split('?',1)
+        else:
+            path,query = self.path,''
+
+        env['PATH_INFO'] = urllib.unquote(path)
+        env['QUERY_STRING'] = query
+
+        host = self.address_string()
+        if host != self.client_address[0]:
+            env['REMOTE_HOST'] = host
+        env['REMOTE_ADDR'] = self.client_address[0]
+
+        if self.headers.typeheader is None:
+            env['CONTENT_TYPE'] = self.headers.type
+        else:
+            env['CONTENT_TYPE'] = self.headers.typeheader
+
+        length = self.headers.getheader('content-length')
+        if length:
+            env['CONTENT_LENGTH'] = length
+
+        for h in self.headers.headers:
+            k,v = h.split(':',1)
+            k=k.replace('-','_').upper(); v=v.strip()
+            if k in env:
+                continue                    # skip content length, type,etc.
+            if 'HTTP_'+k in env:
+                env['HTTP_'+k] += ','+v     # comma-separate multiple headers
+            else:
+                env['HTTP_'+k] = v
+        return env
+
+    def get_stderr(self):
+        return sys.stderr
+
+    def handle(self):
+        """Handle a single HTTP request"""
+
+        self.raw_requestline = self.rfile.readline()
+        if not self.parse_request(): # An error code has been sent, just exit
+            return
+
+        handler = ServerHandler(
+            self.rfile, self.wfile, self.get_stderr(), self.get_environ()
+        )
+        handler.request_handler = self      # backpointer for logging
+        handler.run(self.server.get_app())
+
+
+class Server(object):
+    def __init__(self, app, initial_environ=None):
+        self.app = app
+        self.base_environ = {} if initial_environ is None else initial_environ.copy()
+        self.base_environ.update({
+            'SERVER_NAME': 'localhost', # TODO: should this be configurable?
+            'SERVER_PORT': '0', # TODO: What?
+            'SCRIPT_NAME': ''
+        })
+
+    def get_app(self):
+        return self.app
 
 def host(app, bind_to):
     acceptor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -84,7 +160,7 @@ def host(app, bind_to):
         acceptor.close()
         print "{0} {1} has left the building.".format(process_info['name'], os.getpid())
 
-    for i in range(3):
+    for i in range(30):
         @fork
         def child():
             process_info['name'] = 'Child'
@@ -98,21 +174,21 @@ def host(app, bind_to):
             acceptor.settimeout(0.0)
             rlist = [acceptor]
 
+            server = Server(app, {
+                'odnorog.process_name': process_info['name']
+            })
+
             while True:
                 for ready_sock in rlist:
                     try:
-                        sock, _ = ready_sock.accept()
+                        sock, client_address = ready_sock.accept()
                     except IOError as ex:
                         if ex.errno not in (errno.EAGAIN, errno.ECONNABORTED): raise
                     else:
                         # TODO: handle errors!
                         sock.settimeout(None)
                         with closing(sock):
-                            with closing(sock.makefile('wb', 16384)) as sock_output:
-                                with closing(sock.makefile('rb', 16384)) as sock_input:
-                                    handler = WsgiHandler.parse_request(sock_input, sock_output, sys.stderr, server_name, server_port)
-                                    handler.base_env['odnorog.process_name'] = process_info['name']
-                                    handler.run(app)
+                            WSGIRequestHandler(sock, client_address, server)
 
                 # Quit if we see that our parent is gone.
                 if os.getppid() != parent_pid: return
@@ -140,10 +216,10 @@ if __name__ == '__main__':
     from wsgiref.validate import validator
 
     def demo_app(environ, start_response):
-        chunks = ["Hello, I'm {0} {1}".format(environ['odnorog.process_name'], os.getpid())]
-        for k, v in sorted(environ.items()):
-            chunks.append(' = '.join((k, repr(v))))
         start_response('200 OK', [('Content-Type', 'text/plain')])
-        yield '\n'.join(chunks)
+        yield "Hello, I'm {0} {1}\n".format(environ['odnorog.process_name'], os.getpid())
+        for k, v in sorted(environ.items()):
+            yield ' = '.join((k, repr(v)))
+            yield '\n'
 
     host(app=validator(demo_app), bind_to=('0.0.0.0', 8888))
