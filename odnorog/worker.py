92c7b774528ed00dd5ac80bf6d9574c606d66289
Pavel Repin <prepin@gmail.com>
2010-05-04 07:48:01 -0700

    Move worker loop state into locally-scoped state class.
    This is done so that Python closures that are used in this loop refer to the same state instead of copied state.

--- odnorog/worker.py
+++ odnorog/worker.py
@@ -33,71 +33,71 @@ class Worker(object):
         """
         return self.nr == other_nr
 
-    def tickle_tmp(self, m):
-        m = 1 if not m else 0
-        os.fchmod(self.tmp, m)
-        return m
-
     def loop(self):
         """
         Runs inside each forked worker, this sits around and waits
         for connections and doesn't die until the parent dies (or is
         given a INT, QUIT, or TERM signal)
         """
-        alive = {True:True}
+        class State(object):
+            alive = True
+            nr = 0 # this becomes negative if we need to reopen logs
+            m = 0
+            @classmethod
+            def fchmod_tmp(cls):
+                cls.m = 1 if not cls.m else 0
+                os.fchmod(self.tmp, cls.m)
 
         def instant_shutdown(*_):
             os._exit(0)
 
         def graceful_shutdown(*_):
-            alive.clear()
+            State.alive = False
             for s in self.listeners:
                 with no_exceptions():
                     s.close()
 
-        nr = 0 # this becomes negative if we need to reopen logs
         ready = self.listeners
 
         ## closing anything we ioc.select on will raise EBADF
-        #trap(:USR1) { nr = -65536; SELF_PIPE.first.close rescue nil }
+        #trap(:USR1) { State.nr = -65536; SELF_PIPE.first.close rescue nil }
         signal.signal(signal.SIGQUIT, graceful_shutdown)
         map(lambda signum: signal.signal(signum, instant_shutdown), [signal.SIGTERM, signal.SIGINT])
         self.log.info("%s ready.", self)
-        m = 0
 
-        while alive:
+        while State.alive:
             try:
-                #nr < 0 and reopen_worker_logs(worker.nr)
-                nr = 0
+                #State.nr < 0 and reopen_worker_logs(worker.nr)
+                State.nr = 0
 
-                # We're a goner in timeout seconds anyways if tickle_tmp throws,
-                # so don't trap the exception. No-op changes with tickle_tmp don't
+                # We're a goner in timeout seconds anyways if fchmod_tmp() throws,
+                # so don't trap the exception. No-op changes with fchmod_tmp() don't
                 # update ctime on all filesystems; so we change our counter each
                 # and every time (after process_client and before ioc.select).
-                m = self.tickle_tmp(m)
+                State.fchmod_tmp()
 
                 #ready.each do |sock|
                 #  begin
                 #    process_client(sock.accept_nonblock)
-                #    nr += 1
-                #    m = self.tickle_tmp(m)
+                #    State.nr += 1
+                #    State.fchmod_tmp()
                 #  rescue Errno::EAGAIN, Errno::ECONNABORTED
                 #  end
-                #  break if nr < 0
+                #  break if State.nr < 0
                 #end
 
                 ## make the following bet: if we accepted clients this round,
                 ## we're probably reasonably busy, so avoid calling select()
                 ## and do a speculative accept_nonblock on ready listeners
                 ## before we sleep again in select().
-                #redo unless nr == 0 # (nr < 0) => reopen logs
+                #redo unless State.nr == 0 # (nr < 0) => reopen logs
 
                 current_ppid = os.getppid()
                 if self.parent_pid != current_ppid:
                     self.log.info("Parent %s died. %s exiting now.", self.parent_pid, self)
                     return
 
-                m = self.tickle_tmp(m)
+                State.fchmod_tmp()
 
                 try:
                     # timeout used so we can detect parent death:
@@ -107,16 +107,16 @@ class Worker(object):
                     if e == errno.EINTR:
                         ready = self.listeners
                     elif e == errno.EBADF:
-                        if nr >= 0:
+                        if State.nr >= 0:
                             # Terminate the loop due to closure of selected descriptors.
                             # If we're reopening logs, nr will be < 0.
-                            alive.clear()
+                            State.alive = False
                     else:
                         raise
                 else:
                     ready = result[0]
             except Exception:
-                if alive:
+                if State.alive:
                     self.log.exception("Unhandled %s loop exception.", self)
 
         self.log.info("%s complete.", self)
